30cf230619e1f320fe4441f22017a3aeba991714
aman <asaxena7531@gmail.com>
2024-03-21 21:43:37 +0530

    add dialect

--- app/model/index.js
+++ app/model/index.js
@@ -1,4 +1,7 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(process.env.db_url)
+const sequelize = new Sequelize(process.env.db_url,{
+  dialect: 'mysql',
+  dialectModule: require('mysql2'),
+})
 
 module.exports = sequelize;
